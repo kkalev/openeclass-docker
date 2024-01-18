b8d17bc3c639994819a2532e117d9f8edc6a4759
kkalev <kkalev@gmail.com>
2024-01-18 14:11:24 +0200

    update: better config.php (for later)

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
  * Created by install on 2024-01-18 11:10
  * ======================================================== */
 
-$mysqlServer = 'db';
-$mysqlUser = 'eclass';
-$mysqlPassword = 'eclass';
-$mysqlMainDb = 'eclass';
+$mysqlServer = getenv('MYSQL_HOST')?:'db';
+$mysqlUser = getenv('MYSQL_USER')?:'eclass';
+$mysqlPassword = getenv('MYSQL_PASSWORD')?:'eclass';
+$mysqlMainDb = getenv('MYSQL_DATABASE')?:'eclass';
